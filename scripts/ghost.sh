b757df7da2330204527622c65101a6ed13d47a57
liu shiwei <liushiwei@gmail.com>
2019-11-04 21:12:32 +0800

    向目标系统注入ghost.sh 用于生成tar的系统包，可用于刷机

---
+++ scripts/ghost.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+if [ "`which xz`" ] ; then
+  gz=.xz
+  gzrun=xz
+else
+  if [ "`which bzip2`" ] ; then
+    gz=.bz2
+    gzrun=bzip2
+  else
+    gz=.gz
+    gzrun=gzip
+  fi
+fi
+
+dst_file="loongson64_`date +%F`.tar$gz"
+cd /
+echo make /$dst_file
+if [ "`which pv`" ] ; then
+  tar cp --exclude=/proc --exclude=/mnt --exclude=/tmp  --exclude=/lost+found --exclude=/sys --exclude=/media --exclude=/run --exclude=$dst_file * |pv|$gzrun >$dst_file
+else
+  tar cp --exclude=/proc --exclude=/mnt --exclude=/tmp  --exclude=/lost+found --exclude=/sys --exclude=/media --exclude=/run --exclude=$dst_file * |$gzrun >$dst_file
+fi

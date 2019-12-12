81fbdc6da638cf4164962cdb8c5360fdb4b48601
liu shiwei <liushiwei@gmail.com>
2019-12-13 00:54:16 +0800

    调整ghost.sh

--- scripts/ghost.sh
+++ scripts/ghost.sh
@@ -1,23 +1,19 @@
 #/bin/bash
 
-if [ "`which xz`" ] ; then
-  gz=.xz
-  gzrun=xz
-else
-  if [ "`which bzip2`" ] ; then
-    gz=.bz2
-    gzrun=bzip2
-  else
-    gz=.gz
-    gzrun=gzip
-  fi
-fi
-
-dst_file="loongson64_`date +%F`.tar$gz"
+dst_file="loongson64_`date +%Y%m%d`.tar.gz"
+echo make home/$dst_file
+echo proc/* >/tmp/exclude.list
+echo mnt/* >>/tmp/exclude.list
+echo tmp/* >>/tmp/exclude.list
+echo dev/* >>/tmp/exclude.list
+echo lost+found/* >>/tmp/exclude.list
+echo sys/* >>/tmp/exclude.list
+echo media/* >>/tmp/exclude.list
+echo run/* >>/tmp/exclude.list
+echo home/$dst_file >>/tmp/exclude.list
 cd /
-echo make /$dst_file
 if [ "`which pv`" ] ; then
-  tar cp --exclude=/proc --exclude=/mnt --exclude=/tmp  --exclude=/lost+found --exclude=/sys --exclude=/media --exclude=/run --exclude=$dst_file * |pv|$gzrun >$dst_file
+  tar cp --exclude-from=/tmp/exclude.list * |pv|gzip >/home/$dst_file
 else
- tar cpv --exclude=/proc --exclude=/mnt --exclude=/tmp  --exclude=/lost+found --exclude=/sys --exclude=/media --exclude=/run --exclude=$dst_file * |$gzrun >$dst_file
+ tar cpv --exclude-from=/tmp/exclude.list * |gzip >/home/$dst_file
 fi

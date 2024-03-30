c75002f599e52ccc7c55ff913abb89e25a45fec2
liu shiwei <liushiwei@gmail.com>
2024-03-30 13:47:23 +0000

    test script

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+umount_all() {
+umount -f proc dev sys
+rmdir proc sys dev
+}
+
+
+if ! [ -x initrd.tmp ] ; then
+  ./build.sh
+fi
+cd initrd.tmp
+cp -a ../scripts/install scripts
+if ! [ -e dev/sda ] ; then
+mkdir -p dev proc sys
+mount -t proc none proc
+mount -t sysfs none sys
+mount -t devtmpfs none dev
+fi
+trap umount_all EXIT
+chroot . /scripts/install

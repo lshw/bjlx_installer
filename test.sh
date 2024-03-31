bb71856cc640a532f122157c0cca2835694ff3c9
liu shiwei <liushiwei@gmail.com>
2024-03-31 16:06:28 +0000

    显示安装的文件个数

--- test.sh
+++ test.sh
@@ -12,10 +12,11 @@ fi
 cd initrd.tmp
 cp -a ../scripts/install scripts
 if ! [ -e dev/sda ] ; then
-mkdir -p dev proc sys
+mkdir -p dev proc sys tmp
 mount -t proc none proc
 mount -t sysfs none sys
 mount -t devtmpfs none dev
 fi
 trap umount_all EXIT
+rm scripts/reinstall
 chroot . /scripts/install sde
